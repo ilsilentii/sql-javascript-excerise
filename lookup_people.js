ba66c6e6ea45d7ce6404e854bb6088d31d7ac651
ilsilentii <travis.vdm@outlook.com>
2017-09-26 13:04:32 -0600

    refactored code

--- lookup_people.js
+++ lookup_people.js
@@ -12,27 +12,36 @@ const client = new pg.Client({
     ssl: settings.ssl
 });
 
-client.connect((err) => {
+//Connects to Database
+
+client.connect((err, result) => {
     if (err) {
         return console.error("Connection Error", err);
     }
+    readname();
+});
+
+//Query's the database based on user input
+
+function readname() {
     console.log("Searching Database ...")
+
     client.query("SELECT * FROM famous_people WHERE last_name = $1::text", people, (err, result) => {
         if (err) {
             return console.error("error running query", err);
         }
         printname(result);
-
         client.end();
     });
-});
+}
 
+//Prints out the results of the query
 
 function printname(result) {
 
     console.log("Found " + result.rows.length + " person(s) by the name of " + "'" + people + "'")
 
     for (let i in result.rows)
-        console.log("- " + result.rows[i].id + ": " + result.rows[i].first_name + " " + result.rows[i].last_name + ", " + "born " + result.rows[i].birthdate.toISOString().substr(0, 10));
+        console.log("- " + (parseInt(i) + 1) + ": " + result.rows[i].first_name + " " + result.rows[i].last_name + ", " + "born " + result.rows[i].birthdate.toISOString().substr(0, 10));
 
 }

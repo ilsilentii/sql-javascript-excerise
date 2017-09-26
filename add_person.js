d173130583c409176012fb8f148763bc66b8d163
ilsilentii <travis.vdm@outlook.com>
2017-09-26 15:41:39 -0600

    working knex functions for creating and displaying tables

--- add_person.js
+++ add_person.js
@@ -6,16 +6,30 @@ const knex = require('knex')({
     }
 });
 
-const insert = {
-    first_name: process.argv[2],
-    last_name: process.argv[3],
-    birthdate: process.argv[4]
+// Adds information to the database based on the users input
+
+function write_to_database() {
+    if (!process.argv[2] || !process.argv[3] || !process.argv[4]) {
+        console.log("Pleas enter correct information")
+        knex.destroy();
+    } else {
+
+        const insert = {
+            first_name: process.argv[2],
+            last_name: process.argv[3],
+            birthdate: process.argv[4]
+        }
+
+        knex.insert(insert).into("famous_people").finally(function() {
+            get_table();
+            knex.destroy();
+        });
+    }
 }
 
-knex.insert(insert).into("famous_people").finally(function() {
-    get_table();
-    knex.destroy();
-});
+write_to_database()
+
+//This function displays the information in the current database
 
 function get_table() {
     knex.select('*').from('famous_people')
